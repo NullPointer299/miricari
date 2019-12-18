af3024b014e7bd3f65ecfad5becac718cf9c4f59
nullpo299 <nullpointer299@gmail.com>
2019-12-18 23:30:02 +0900

    First commit

---
+++ src/main/java/com/fjb3u/miricari/MiricariApplication.java
@@ -0,0 +1,13 @@
+package com.fjb3u.miricari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiricariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiricariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fjb3u/miricari/MiricariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjb3u.miricari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiricariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
