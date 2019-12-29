cf20265a84dfa998f4bdfed389ab9401712fcc45
nullpo299 <nullpointer299@gmail.com>
2019-12-29 20:30:55 +0900

    すべてのページに飛べるようにしました

--- src/main/java/com/fjb3u/miricari/controller/MiricariController.java
+++ src/main/java/com/fjb3u/miricari/controller/MiricariController.java
@@ -1,7 +1,73 @@
 package com.fjb3u.miricari.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/miricari")
 @Controller
 public class MiricariController {
+
+    @RequestMapping("/productlist")
+    public String getProductList() {
+        return "productlist";
+    }
+
+    @RequestMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @RequestMapping("/mypage")
+    public String getMypage() {
+        return "mypage";
+    }
+
+    @RequestMapping("/exhibit")
+    public String getExhibit() {
+        return "exhibit";
+    }
+
+    @RequestMapping("/logout")
+    public String logout() {
+        // ログアウト処理
+        return getProductList();
+    }
+
+    @RequestMapping("/cart")
+    public String getCart() {
+        return "cart";
+    }
+
+    @RequestMapping("/auth")
+    public String auth(final ModelAndView mav) {
+        String url = "mypage";
+        // ログイン処理
+        return url;
+    }
+
+    @RequestMapping("/accountinfo")
+    public String getAccountinfo() {
+        return "accountinfo";
+    }
+
+    @RequestMapping("/detail")
+    public String getDetail() {
+        return "productdetails";
+    }
+
+    @RequestMapping("/appendcart")
+    public String appendCart() {
+        return "cart";
+    }
+
+    @RequestMapping("/purchase")
+    public String purchase() {
+        return "purchase";
+    }
+
+    @RequestMapping("/complete")
+    public String complete() {
+        return "purchasecomplete";
+    }
 }
